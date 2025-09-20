37e37c9b55c84b312756c80f06c0e475c15d698c
Luca Tabbia <luca.tabbia97@gmail.com>
2025-09-20 16:50:59 +0200

    chore: added last file lint

--- backend/src/controllers/files/file-filter.spec.ts
+++ backend/src/controllers/files/file-filter.spec.ts
@@ -2,41 +2,41 @@ import { BadRequestException } from '@nestjs/common';
 import type { FileFilterCallback } from 'multer';
 
 const fileFilter = (
-    _req: Express.Request,
-    file: Express.Multer.File,
-    callback: FileFilterCallback,
+  _req: Express.Request,
+  file: Express.Multer.File,
+  callback: FileFilterCallback,
 ) => {
-    const allowedMimeTypes = [
-        'image/jpeg',
-        'image/png',
-        'application/pdf',
-        'text/plain',
-    ];
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        callback(null, true);
-    } else {
-        callback(new BadRequestException('The file type is not supported'));
-    }
+  const allowedMimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'application/pdf',
+    'text/plain',
+  ];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    callback(null, true);
+  } else {
+    callback(new BadRequestException('The file type is not supported'));
+  }
 };
 
 describe('File Filter', () => {
-    it('should call the callback with true for a supported file type', () => {
-        const mockFile = { mimetype: 'image/jpeg' } as Express.Multer.File;
-        const callback = jest.fn();
+  it('should call the callback with true for a supported file type', () => {
+    const mockFile = { mimetype: 'image/jpeg' } as Express.Multer.File;
+    const callback = jest.fn();
 
-        fileFilter({} as Express.Request, mockFile, callback);
+    fileFilter({} as Express.Request, mockFile, callback);
 
-        expect(callback).toHaveBeenCalledWith(null, true);
-    });
+    expect(callback).toHaveBeenCalledWith(null, true);
+  });
 
-    it('should call the callback with an error for an unsupported file type', () => {
-        const mockFile = { mimetype: 'image/avif' } as Express.Multer.File;
-        const callback = jest.fn();
+  it('should call the callback with an error for an unsupported file type', () => {
+    const mockFile = { mimetype: 'image/avif' } as Express.Multer.File;
+    const callback = jest.fn();
 
-        fileFilter({} as Express.Request, mockFile, callback);
+    fileFilter({} as Express.Request, mockFile, callback);
 
-        expect(callback).toHaveBeenCalledWith(
-            new BadRequestException('The file type is not supported'),
-        );
-    });
+    expect(callback).toHaveBeenCalledWith(
+      new BadRequestException('The file type is not supported'),
+    );
+  });
 });
