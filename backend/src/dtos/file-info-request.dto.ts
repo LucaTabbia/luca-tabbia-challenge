4453cf6c5b8a451e73eced300dd212044f6e6cb6
Luca Tabbia <luca.tabbia97@gmail.com>
2025-09-21 20:22:46 +0200

    chore: last file lint

--- backend/src/dtos/file-info-request.dto.ts
+++ backend/src/dtos/file-info-request.dto.ts
@@ -1,40 +1,40 @@
 import { Expose } from 'class-transformer';
 
 export interface IFileInfoRequestDto {
-    userId: string;
-    key: string;
-    name: string;
-    size: number;
-    mimetype: string;
+  userId: string;
+  key: string;
+  name: string;
+  size: number;
+  mimetype: string;
 }
 
 export class FileInfoRequestDto implements IFileInfoRequestDto {
-    @Expose()
-    userId: string;
+  @Expose()
+  userId: string;
 
-    @Expose()
-    key: string;
+  @Expose()
+  key: string;
 
-    @Expose()
-    name: string;
+  @Expose()
+  name: string;
 
-    @Expose()
-    size: number;
+  @Expose()
+  size: number;
 
-    @Expose()
-    mimetype: string;
+  @Expose()
+  mimetype: string;
 
-    constructor(
-        userId: string,
-        key: string,
-        name: string,
-        size: number,
-        mimetype: string,
-    ) {
-        this.userId = userId;
-        this.key = key;
-        this.name = name;
-        this.size = size;
-        this.mimetype = mimetype;
-    }
+  constructor(
+    userId: string,
+    key: string,
+    name: string,
+    size: number,
+    mimetype: string,
+  ) {
+    this.userId = userId;
+    this.key = key;
+    this.name = name;
+    this.size = size;
+    this.mimetype = mimetype;
+  }
 }
